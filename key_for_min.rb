dca0da44ceb9734fa9e1a88bac1b80c231abc0f0
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-20 18:32:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest = 9999
+  name_hash.each do |key, value|
+    if value < smellest
+      smallest = key
+    else nil
+    end
+  end
+  smallest
 end
