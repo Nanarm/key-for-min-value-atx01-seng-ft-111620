2d7aab8fe78e5049e336795d251b6bfb4aa6250c
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-20 18:37:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
     if value < smallest
       smallest = value
       result = key
-    else if name_hash == " "
+    elif name_hash == " "
       return nil
     end
   end
